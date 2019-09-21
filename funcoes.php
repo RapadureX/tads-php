ce347a732c61d4f61b59acd6fd0f8e1375204855
Vicente <vicnet@gmail.com>
2019-09-20 21:40:21 -0300

    first commit

---
+++ funcoes.php
@@ -0,0 +1,17 @@
+<?php
+    function viewProduto($produto){
+        list(
+            "item"=>$item,
+            "valor"=>$valor,
+            "quantidade"=>$quantidade
+            )=$produto;
+        $total=$valor*$quantidade;
+        return <<<STR
+            <tr>
+                <th>{$item}</th>
+                <td>{$valor}</td>
+                <td>{$quantidade}</td>
+                <td>{$total}</td>
+            </tr>
+        STR;
+    }
